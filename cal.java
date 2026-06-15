d3f8d6c89292f3b3b6f254a92023dbcef66299d5
Bhargavi-0106 <katambhargavi001@gmail.com>
2026-06-15 19:29:27 +0530

    Add files via upload

---
+++ cal.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+
+        System.out.println("Addition: " + (n1 + n2));
+        System.out.println("Subtraction: " + (n1 - n2));
+        System.out.println("Multiplication: " + (n1 * n2));
+        System.out.println("Division: " + (n1 / n2));
+    }
+}
